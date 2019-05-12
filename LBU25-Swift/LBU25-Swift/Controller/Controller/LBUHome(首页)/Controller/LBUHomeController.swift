5e3cbf92eddc3775b038b3fd358bc61660832d37
liubo <2281075105@qq.com>
2019-05-13 07:27:06 +0800

    添加搜索

--- LBU25-Swift/LBU25-Swift/Controller/Controller/LBUHome(首页)/Controller/LBUHomeController.swift
+++ LBU25-Swift/LBU25-Swift/Controller/Controller/LBUHome(首页)/Controller/LBUHomeController.swift
@@ -12,19 +12,18 @@ class LBUHomeController: LBUPageController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func configNavigationBar() {
+        super.configNavigationBar()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "nav_search"),
+                                                            target: self,
+                                                            action: #selector(searchButtonClick))
     }
-    */
+    
+    @objc private func searchButtonClick() {
+        navigationController?.pushViewController(LBUBaseSearchController(), animated: true)
+    }
 
 }
